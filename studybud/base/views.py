324444a7633bfe695981fbfe00ce227491297a47
Isalifestyle <oisa@uncc.edu>
2025-01-26 20:27:57 -0500

    search

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import Room
+from .models import Room, Topic
 from .forms import RoomForm
 
 def home(request):
     rooms = Room.objects.all()
+
+    topic = Topic.objects.all()
     context = {'rooms': rooms}
     return render(request, 'base/home.html', context)
 
@@ -30,5 +32,18 @@ def createRoom(request):
 def updateRoom(request, pk):
     room = Room.objects.get(id = pk)
     form = RoomForm(instance = room)
+
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance = room)
+        if form.is_valid:
+            form.save()
+            return redirect('home')
     context = {'form':form}
     return render(request, 'base/room_form.html', context)
+
+def deleteRoom(request, pk):
+    room = Room.objects.get(id = pk)
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+    return render(request, 'base/delete.html', {'obj': room})
