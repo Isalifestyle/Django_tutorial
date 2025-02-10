9967b28b7f67b42ad63fe7bd9bdcfa2d18a41c8c
Isalifestyle <oisa@uncc.edu>
2025-02-10 17:56:52 -0500

    Mobile Responsiveness

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from .models import Room, Topic, Message, User
-from .forms import RoomForm
+from .forms import RoomForm, UserForm
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
@@ -151,4 +151,16 @@ def deleteMessage(request, pk):
         message.delete()
         return redirect('home')
     return render(request, 'base/delete.html', {'obj': message})
+
+@login_required(login_url='login')
+def updateUser(request):
+    user = request.user
+    form = UserForm(instance = user)
+
+    if request.method == 'POST':
+        form = UserForm(request.POST, instance = user)
+        if form.is_valid():
+            form.save()
+            return redirect('user-profile', pk = user.id)
+    return render(request, 'base/update-user.html', {'form':form})
 
