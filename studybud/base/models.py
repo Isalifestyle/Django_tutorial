85567e05d9da77681d4a9d5ae2e50657b9b2cdd6
Isalifestyle <oisa@uncc.edu>
2025-02-17 08:42:20 -0500

    Integrating Custom User Model (finished)

--- studybud/base/models.py
+++ studybud/base/models.py
@@ -1,6 +1,21 @@
 from django.db import models
-from django.contrib.auth.models import User
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    name = models.CharField(max_length=200, null=True)
+    email = models.EmailField(unique=True, null=True)
+    username = models.CharField(max_length=200, unique=True)
+    bio = models.TextField(null=True)
+    avatar = models.ImageField(null=True, default="avatar.svg")
+    # location = models.CharField(max_length=200, null=True)
+    # website = models.CharField(max_length=200, null=True)
+    # social_github = models.CharField(max_length=200, null=True)
+    # social_twitter = models.CharField(max_length=200, null=True)
+    # social_linkedin = models.CharField(max_length=200, null=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
 class Topic(models.Model):
     name = models.CharField(max_length = 200)
 
